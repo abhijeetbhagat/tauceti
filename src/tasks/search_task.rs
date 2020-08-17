e81b442dfe3c083d1abbd0e66100e31d80a91f77
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-08-17 11:38:09 +0530

    Add test case for search task

--- src/tasks/search_task.rs
+++ src/tasks/search_task.rs
@@ -19,15 +19,24 @@ impl TaucetiTask for SearchTask {
 
 pub async fn search(
     index_tree: Arc<RwLock<IndexTree<String, u32>>>,
-    doc_id: u32,
+    terms: &[&str],
 ) -> Result<Vec<u32>, std::io::Error> {
     let guard = index_tree.read().await;
-    //let result: Vec<u32> = guard.query();
-    //Ok(result)
-    todo!()
+    let result: Vec<u32> = guard.query(terms);
+    Ok(result)
 }
 
 #[async_std::test]
 async fn test_searching() -> std::io::Result<()> {
+    let tree = Arc::new(RwLock::new(IndexTree::new()));
+    {
+        let mut guard = tree.write().await;
+        guard.insert("c++".into(), 1);
+        guard.insert("c++".into(), 2);
+        guard.insert("python".into(), 1);
+        guard.insert("java".into(), 3);
+    }
+
+    assert_eq!(search(tree, &["c++"]).await.unwrap().len(), 2);
     Ok(())
 }
