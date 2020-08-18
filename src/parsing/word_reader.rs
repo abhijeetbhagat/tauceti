cb03a623ffbb7e8e495365d3142f52a087a10d6c
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-08-18 16:23:53 +0530

    Add support for docx files

--- src/parsing/word_reader.rs
+++ src/parsing/word_reader.rs
@@ -1,6 +1,10 @@
+extern crate dotext;
 use super::reader::DocReader;
+use crate::parsing::word_reader::dotext::MsDoc;
 use async_std::path::PathBuf;
 use async_trait::async_trait;
+use dotext::Docx;
+use std::io::Read;
 
 /// A PDF file reader
 pub struct WordReader {
@@ -16,6 +20,17 @@ impl WordReader {
 #[async_trait]
 impl DocReader for WordReader {
     async fn parse(&mut self, _: async_std::path::PathBuf) -> Result<String, std::io::Error> {
-        todo!()
+        let mut file = Docx::open(&self.path).unwrap();
+        let mut content = String::new();
+        let _ = file.read_to_string(&mut content);
+        Ok(content)
     }
+}
+
+#[async_std::test]
+async fn test_doc_reading() -> std::io::Result<()> {
+    let mut word_reader = WordReader::new("test-resume.docx");
+    let content = word_reader.parse("".into()).await.unwrap();
+    assert_eq!(content, "\n\ncpp python irrelevant java");
+    Ok(())
 }
